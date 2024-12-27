4169eea49f40fd11b59711f05bcbef04451b2a25
Murat Okçu <mrtkcu0101@gmail.com>
2024-12-27 11:52:25 +0300

    Fix Vite app port binding

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // This makes the app accessible externally (important for cloud platforms)
+    port: process.env.PORT || 3000, // Use the port assigned by Render or default to 3000
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
-    reporters: ['json', 'verbose'],
-    outputFile: './resultz.json',
+    environment: "jsdom",
+    reporters: ["json", "verbose"],
+    outputFile: "./resultz.json",
   },
 });
